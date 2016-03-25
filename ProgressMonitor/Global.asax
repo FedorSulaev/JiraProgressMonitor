39c7b7c15aaec8c2675819ce5a63e55db9fbdf63
FedorSulaev <fedor.sulaev@outlook.com>
2016-03-25 18:02:30 +0300

    feat: MVC project template with auth

---
+++ ProgressMonitor/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProgressMonitor.MvcApplication" Language="C#" %>
---
+++ ProgressMonitor/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProgressMonitor.Startup))]
+namespace ProgressMonitor
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
