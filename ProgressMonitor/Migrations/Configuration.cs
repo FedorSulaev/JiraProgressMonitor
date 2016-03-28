66d93d5af0e1d1cb3c914543388ba8fdc57db1fe
FedorSulaev <fedor.sulaev@outlook.com>
2016-03-28 14:17:54 +0300

    fix: Default user seeding
    Enabled lazyloading and removed async calls from seed method.

--- ProgressMonitor/Migrations/Configuration.cs
+++ ProgressMonitor/Migrations/Configuration.cs
@@ -15,7 +15,7 @@ namespace ProgressMonitor.Migrations
             AutomaticMigrationsEnabled = false;
         }
 
-        protected override async void Seed(ProgressMonitor.Models.ApplicationDbContext context)
+        protected override void Seed(ApplicationDbContext context)
         {
 			//  This method will be called after migrating to the latest version.
 
@@ -29,21 +29,22 @@ namespace ProgressMonitor.Migrations
 			//      new Person { FullName = "Rowan Miller" }
 			//    );
 			//
+	        context.Configuration.LazyLoadingEnabled = true;
 			RoleStore<IdentityRole> roleStore = new RoleStore<IdentityRole>(context);
 			RoleManager<IdentityRole> roleManager = new RoleManager<IdentityRole>(roleStore);
 			if (!context.Roles.Any(r => r.Name == UserRoles.AdminRole))
 	        {
 				IdentityRole role = new IdentityRole {Name = UserRoles.AdminRole};
-		        await roleManager.CreateAsync(role);
+		        roleManager.Create(role);
 	        }
 	        string adminRoleId = roleManager.Roles.First(r => r.Name == UserRoles.AdminRole).Id;
-	        if (!context.Users.Any(u => u.Roles.Any(r => r.RoleId == adminRoleId)))
+	        if (!context.Users.Any() || !context.Users.Any(u => u.Roles.Any(r => r.RoleId == adminRoleId)))
 	        {
 		        UserStore<ApplicationUser> userStore = new UserStore<ApplicationUser>(context);
 				UserManager<ApplicationUser> userManager = new UserManager<ApplicationUser>(userStore);
 		        ApplicationUser adminUser = new ApplicationUser {UserName = "admin"};
-		        await userManager.CreateAsync(adminUser, "0000");
-		        await userManager.AddToRoleAsync(adminUser.Id, UserRoles.AdminRole);
+		        userManager.Create(adminUser, "password");
+				userManager.AddToRole(adminUser.Id, UserRoles.AdminRole);
 	        }
         }
     }
