2c836207b7361ce9ca01f2ad545131a111410ccb
FedorSulaev <fedor.sulaev@outlook.com>
2016-03-28 13:40:51 +0300

    feat: Default admin user

---
+++ ProgressMonitor/Constants/UserRoles.cs
@@ -0,0 +1,8 @@
+﻿namespace ProgressMonitor.Constants
+{
+	public static class UserRoles
+	{
+		public const string AdminRole = "Admin";
+		public const string ClientRole = "Client";
+	}
+}
--- ProgressMonitor/Migrations/Configuration.cs
+++ ProgressMonitor/Migrations/Configuration.cs
@@ -1,3 +1,9 @@
+using System.Linq;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using ProgressMonitor.Constants;
+using ProgressMonitor.Models;
+
 namespace ProgressMonitor.Migrations
 {
 	using System.Data.Entity.Migrations;
@@ -9,20 +15,36 @@ namespace ProgressMonitor.Migrations
             AutomaticMigrationsEnabled = false;
         }
 
-        protected override void Seed(ProgressMonitor.Models.ApplicationDbContext context)
+        protected override async void Seed(ProgressMonitor.Models.ApplicationDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
+			//  This method will be called after migrating to the latest version.
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+			//  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+			//  to avoid creating duplicate seed data. E.g.
+			//
+			//    context.People.AddOrUpdate(
+			//      p => p.FullName,
+			//      new Person { FullName = "Andrew Peters" },
+			//      new Person { FullName = "Brice Lambson" },
+			//      new Person { FullName = "Rowan Miller" }
+			//    );
+			//
+			RoleStore<IdentityRole> roleStore = new RoleStore<IdentityRole>(context);
+			RoleManager<IdentityRole> roleManager = new RoleManager<IdentityRole>(roleStore);
+			if (!context.Roles.Any(r => r.Name == UserRoles.AdminRole))
+	        {
+				IdentityRole role = new IdentityRole {Name = UserRoles.AdminRole};
+		        await roleManager.CreateAsync(role);
+	        }
+	        string adminRoleId = roleManager.Roles.First(r => r.Name == UserRoles.AdminRole).Id;
+	        if (!context.Users.Any(u => u.Roles.Any(r => r.RoleId == adminRoleId)))
+	        {
+		        UserStore<ApplicationUser> userStore = new UserStore<ApplicationUser>(context);
+				UserManager<ApplicationUser> userManager = new UserManager<ApplicationUser>(userStore);
+		        ApplicationUser adminUser = new ApplicationUser {UserName = "admin"};
+		        await userManager.CreateAsync(adminUser, "0000");
+		        await userManager.AddToRoleAsync(adminUser.Id, UserRoles.AdminRole);
+	        }
         }
     }
 }
