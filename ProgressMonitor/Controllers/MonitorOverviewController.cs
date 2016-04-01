7a9a941277de0a4cb6a20733efebc2e955a95e08
FedorSulaev <fedor.sulaev@outlook.com>
2016-04-01 17:40:16 +0300

    feat: Overview page view and controller

---
+++ ProgressMonitor/Controllers/MonitorOverviewController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProgressMonitor.Controllers
+{
+    public class MonitorOverviewController : Controller
+    {
+        // GET: MonitorOverview
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
