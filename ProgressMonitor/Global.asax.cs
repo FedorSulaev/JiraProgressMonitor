fa24415fb3c51c5bb3dd9dae6d82ac5e610a8c2b
FedorSulaev <fedor.sulaev@outlook.com>
2016-04-04 17:37:58 +0300

    feat: DependencyResolver initialization

--- ProgressMonitor/Global.asax.cs
+++ ProgressMonitor/Global.asax.cs
@@ -1,6 +1,8 @@
 ﻿using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Autofac;
+using Autofac.Integration.Mvc;
 
 namespace ProgressMonitor
 {
@@ -12,6 +14,15 @@ namespace ProgressMonitor
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+	        DependencyResolver.SetResolver(CreateAutofacDependencyResolver());
         }
+
+	    private IDependencyResolver CreateAutofacDependencyResolver()
+	    {
+		    ContainerBuilder builder = new ContainerBuilder();
+		    builder.RegisterControllers(typeof(MvcApplication).Assembly).PropertiesAutowired();
+		    IContainer container = builder.Build();
+			return new AutofacDependencyResolver(container);
+	    }
     }
 }
