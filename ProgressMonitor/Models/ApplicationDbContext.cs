488e86b54c21d2c196f534ef418fd63bd35a727b
FedorSulaev <fedor.sulaev@outlook.com>
2016-04-05 20:13:55 +0300

    style: ApplicationDbContext in a separate file

---
+++ ProgressMonitor/Models/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace ProgressMonitor.Models
+{
+	public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+	{
+		public ApplicationDbContext()
+			: base("DefaultConnection", throwIfV1Schema: false)
+		{
+			
+		}
+
+		public static ApplicationDbContext Create()
+		{
+			return new ApplicationDbContext();
+		}
+	}
+}
