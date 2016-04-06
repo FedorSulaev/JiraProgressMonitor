e9824f66efe14dd056cbe053be42149aeb634483
FedorSulaev <fedor.sulaev@outlook.com>
2016-04-06 18:43:55 +0300

    feat: Basic auth credentials encoding

--- ProgressMonitor/Services/JiraAPIService.cs
+++ ProgressMonitor/Services/JiraAPIService.cs
@@ -1,6 +1,15 @@
-﻿namespace ProgressMonitor.Services
+﻿using System;
+using System.Text;
+
+namespace ProgressMonitor.Services
 {
 	public class JiraAPIService : IJiraAPIService
 	{
+		private string EncodeCredentials(string username, string password)
+		{
+			string mergedCredentials = $"{username}:{password}";
+			byte[] byteCredentials = Encoding.UTF8.GetBytes(mergedCredentials);
+			return Convert.ToBase64String(byteCredentials);
+		}
 	}
 }
