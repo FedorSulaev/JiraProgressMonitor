84bdfbbe3b7c70c67a9017a387f7d4ab268d2902
FedorSulaev <fedor.sulaev@outlook.com>
2016-04-08 19:10:35 +0300

    feat: Project serialization model

---
+++ ProgressMonitor/Models/JsonSerialization/Project.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace ProgressMonitor.Models.JsonSerialization
+{
+	public class Project
+	{
+		[JsonProperty("self")]
+		public string Self { get; set; }
+
+		[JsonProperty("id")]
+		public long Id  { get; set; }
+
+		[JsonProperty("key")]
+		public string Key { get; set; }
+
+		[JsonProperty("name")]
+		public string Name { get; set; }
+
+		[JsonProperty("avatarUrls")]
+		public Dictionary<string, string> AvatarUrls { get; set; } 
+	}
+}
