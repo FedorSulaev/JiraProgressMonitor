f63f3ed6fa3d2646203b89024c0649da4916c4d8
FedorSulaev <fedor.sulaev@outlook.com>
2016-04-19 18:34:55 +0300

    feat: Action url for versions

---
+++ ProgressMonitor/Controllers/VersionController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace ProgressMonitor.Controllers
+{
+    public class VersionController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
