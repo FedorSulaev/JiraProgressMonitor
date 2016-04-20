bfc054cdada72b40bc88d3f65927c72fe4e94392
FedorSulaev <fedor.sulaev@outlook.com>
2016-04-20 17:33:22 +0300

    feat: Issue basic model

---
+++ ProgressMonitor/Models/JsonSerialization/Issue.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace ProgressMonitor.Models.JsonSerialization
+{
+	public class Issue
+	{
+		[JsonProperty("id")]
+		public long Id { get; set; }
+
+		[JsonProperty("key")]
+		public string Key { get; set; }
+	}
+}
