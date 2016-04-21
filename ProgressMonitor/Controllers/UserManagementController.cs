e7622abc330ffa6367965514d81ed8a200074b8f
FedorSulaev <fedor.sulaev@outlook.com>
2016-04-21 19:23:11 +0300

    feat: User management controller

---
+++ ProgressMonitor/Controllers/UserManagementController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+using ProgressMonitor.Constants;
+
+namespace ProgressMonitor.Controllers
+{
+	[Authorize(Roles = UserRoles.AdminRole)]
+    public class UserManagementController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
