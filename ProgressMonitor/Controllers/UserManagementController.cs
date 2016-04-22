b61c8a0d6f0c5c8512326a6dc3a18a2524213b76
FedorSulaev <fedor.sulaev@outlook.com>
2016-04-22 12:24:31 +0300

    feat: List of users on user management page

--- ProgressMonitor/Controllers/UserManagementController.cs
+++ ProgressMonitor/Controllers/UserManagementController.cs
@@ -1,14 +1,24 @@
-﻿using System.Web.Mvc;
+﻿using System.Linq;
+using System.Web.Mvc;
 using ProgressMonitor.Constants;
+using ProgressMonitor.Models;
 
 namespace ProgressMonitor.Controllers
 {
 	[Authorize(Roles = UserRoles.AdminRole)]
     public class UserManagementController : Controller
     {
-        public ActionResult Index()
-        {
-            return View();
+		private readonly ApplicationDbContext _context;
+
+		public UserManagementController()
+		{
+			_context = new ApplicationDbContext();
+		}
+
+		public ActionResult Index()
+		{
+			var model = _context.Users.Select(u => u.UserName).ToList();
+            return View(model);
         }
     }
 }
