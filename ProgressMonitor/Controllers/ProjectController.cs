854ee0a5eed025f3a13e4b1e573b327b079ae538
FedorSulaev <fedor.sulaev@outlook.com>
2016-05-05 11:47:15 +0300

    fix: Resolves #1

--- ProgressMonitor/Controllers/ProjectController.cs
+++ ProgressMonitor/Controllers/ProjectController.cs
@@ -1,4 +1,7 @@
-﻿using System.Web.Mvc;
+﻿using System.Linq;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using ProgressMonitor.Models.DbModels;
 using ProgressMonitor.Services;
 
 namespace ProgressMonitor.Controllers
@@ -7,14 +10,22 @@ namespace ProgressMonitor.Controllers
     public class ProjectController : Controller
     {
 	    private readonly IJiraAPIService _jiraAPIService;
+		private readonly ApplicationDbContext _context;
 
 	    public ProjectController(IJiraAPIService jiraAPIService)
 	    {
 		    _jiraAPIService = jiraAPIService;
+			_context = new ApplicationDbContext();
 	    }
 
 	    public ActionResult Index(long id)
 	    {
+		    string userId = User.Identity.GetUserId();
+		    if (_context.Users.First(u => u.Id == userId)
+				.AccessibleProjects.All(p => p.JiraId != id))
+		    {
+			    return null;
+		    }
 		    var model = _jiraAPIService.GetProject(id);
             return View(model);
         }
--- ProgressMonitor/Controllers/VersionController.cs
+++ ProgressMonitor/Controllers/VersionController.cs
@@ -1,4 +1,7 @@
-﻿using System.Web.Mvc;
+﻿using System.Linq;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using ProgressMonitor.Models.DbModels;
 using ProgressMonitor.Services;
 
 namespace ProgressMonitor.Controllers
@@ -7,15 +10,23 @@ namespace ProgressMonitor.Controllers
     public class VersionController : Controller
     {
 	    private readonly IJiraAPIService _jiraAPIService;
+		private readonly ApplicationDbContext _context;
 
 	    public VersionController(IJiraAPIService jiraAPIService)
 	    {
 		    _jiraAPIService = jiraAPIService;
+			_context = new ApplicationDbContext();
 	    }
 
-	    public ActionResult Index(long id)
+	    public ActionResult Index(long versionId, long projectId)
 	    {
-		    var model = _jiraAPIService.GetIssuesByVersion(id);
+		    string userId = User.Identity.GetUserId();
+			if (_context.Users.First(u => u.Id == userId)
+				.AccessibleProjects.All(p => p.JiraId != projectId))
+			{
+				return null;
+			}
+			var model = _jiraAPIService.GetIssuesByVersion(versionId);
             return View(model);
         }
     }
